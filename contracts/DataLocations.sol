995216ce11f8492d5af751ac1e6eaa75bba70a5a
jungdawoon <tyranno2020@gmail.com>
2023-09-22 14:34:59 +0900

    homework

--- contracts/DataLocations.sol
+++ contracts/DataLocations.sol
@@ -13,44 +13,39 @@ contract DataLocations {
     uint256 public nextUserId = 1;
 
     function addUser(string memory _name, bool _isActive) public returns (uint256) {
-        //fill in (hint "users[nextUserId] = ~ ")
+        users[nextUserId] = User(_name, _isActive, new uint256[](0));
         nextUserId++;
         return nextUserId - 1;
     }
 
     function updateUser(string calldata _newName, bool _isActive, uint256 _userId) external {
         require(users[_userId].isActive, "User is not active");
-
-        User memory userToUpdate = users[_userId]; //something wrong
+        User storage userToUpdate = users[_userId];
         userToUpdate.name = _newName;
         userToUpdate.isActive = _isActive;
     }
 
     function deleteUser(uint256 _userId) external {
         require(users[_userId].isActive, "User is not active");
-
-        //fill in
+        delete(users[_userId]);
     }
 
     function addScore(uint256 _userId, uint256 _score) public {
         require(users[_userId].isActive, "User is not active");
-
-        //fill in
+        User storage userToUpdate = users[_userId];
+        userToUpdate.scores.push(_score);
     }
 
     function getScore(uint256 _userId, uint256 _index) public view returns (uint256) {
         require(_index < users[_userId].scores.length, "Index out of bounds");
-        //fill in
+        return users[_userId].scores[_index];
     }
 
     function transferScore(uint256 _fromUserId, uint256 _toUserId, uint256 _index) public {
         require(users[_fromUserId].isActive && users[_toUserId].isActive, "Either of the user is not active");
-
-        //Have to define 'fromUser' (User type)
-        //Have to define 'toUser' (User type)
-
+        User storage fromUser = users[_fromUserId];
+        User storage toUser = users[_toUserId];
         require(_index < fromUser.scores.length, "Index out of bounds");
-        
         toUser.scores.push(fromUser.scores[_index]);
         delete fromUser.scores[_index];
     }
